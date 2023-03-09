d39a5a3528f927d4c3a9464f0f75d3fec08f63b6
tvaira <tvaira@free.fr>
2023-03-09 19:34:41 +0100

    BASKET2023-24 Formatage du code

--- QT/Basket-GUI/basketgame.cpp
+++ QT/Basket-GUI/basketgame.cpp
@@ -1,23 +1,22 @@
 #include "basketgame.h"
 #include "ui_basketgame.h"
 
-BasketGame::BasketGame(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::BasketGame)
+BasketGame::BasketGame(QWidget* parent) :
+    QMainWindow(parent), ui(new Ui::BasketGame)
 {
     ui->setupUi(this);
 }
+
 BasketGame::~BasketGame()
 {
     delete ui;
 }
-    void BasketGame::paintEvent(QPaintEvent *event)
+
+void BasketGame::paintEvent(QPaintEvent* event)
 {
-
-  QPainter painter(this);
-  QBrush background(QColor(23, 23, 34));
-  painter.setBrush(background);
-  painter.setPen  (Qt::NoPen ); // No stroke
-  painter.drawRect(0, 0, width(), height());
-
+    QPainter painter(this);
+    QBrush   background(QColor(23, 23, 34));
+    painter.setBrush(background);
+    painter.setPen(Qt::NoPen); // No stroke
+    painter.drawRect(0, 0, width(), height());
 }
--- QT/Basket-GUI/basketgame.h
+++ QT/Basket-GUI/basketgame.h
@@ -1,24 +1,26 @@
 #ifndef BASKETGAME_H
 #define BASKETGAME_H
 
-#include <QMainWindow>
-#include <QDesktopWidget>
-#include <QWidget>
-#include <QtGui>
+#include <QtWidgets>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class BasketGame; }
+namespace Ui
+{
+class BasketGame;
+}
 QT_END_NAMESPACE
 
 class BasketGame : public QMainWindow
 {
     Q_OBJECT
 
-public:
-    BasketGame(QWidget *parent = nullptr);
+  public:
+    BasketGame(QWidget* parent = nullptr);
     ~BasketGame();
-    void paintEvent(QPaintEvent *event);
-private:
-    Ui::BasketGame *ui;
+    void paintEvent(QPaintEvent* event);
+
+  private:
+    Ui::BasketGame* ui;
 };
+
 #endif // BASKETGAME_H
