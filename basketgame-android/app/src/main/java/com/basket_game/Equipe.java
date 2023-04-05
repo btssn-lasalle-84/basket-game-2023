67affcb1f20168cebfaf0abf4bed94ec3ac3218d
guillaumetflorent <florent.guillaumet.pro@gmail.com>
2023-04-05 15:24:21 +0200

    Création projet android

---
+++ basketgame-android/app/src/main/java/com/basket_game/Equipe.java
@@ -0,0 +1,34 @@
+package com.basket_game;
+
+public class Equipe
+{
+    private String equipe1;
+    private String equipe2;
+
+    public Equipe(String equipe1, String equipe2)
+    {
+        this.equipe1 = equipe1;
+        this.equipe2 = equipe2;
+    }
+
+    public String getEquipe1()
+    {
+        return equipe1;
+    }
+
+    public void setEquipe1(String equipe1)
+    {
+        this.equipe1 = equipe1;
+    }
+
+    public String getEquipe2()
+    {
+        return equipe2;
+    }
+
+    public void setEquipe2(String equipe2)
+    {
+        this.equipe2 = equipe2;
+    }
+}
+
